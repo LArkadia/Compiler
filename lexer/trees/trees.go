069d2dc0b55a9528f89e75d4591a2eb2ab61c8da
LArkadia <lubam1303@outlook.com>
2023-09-22 20:01:46 -0600

    Agregado estructuras y funciones para y los grafos

--- lexer/trees/trees.go
+++ lexer/trees/trees.go
@@ -1,1 +1,71 @@
 package trees
+
+type Node struct{
+	child map[rune]*Node
+	final bool	
+}
+
+func (root *Node) Add(sequence []rune)  {
+	if root.child == nil{
+		root.child = make(map[rune]*Node)
+	}
+	switch{
+	case len(sequence) > 0:
+		if root.child[sequence[0]] == nil{
+			root.child[sequence[0]] = new(Node)
+		}
+		root.child[sequence[0]].Add(sequence[1:])
+	default:
+		root.final = true
+		return
+	}
+}
+
+func (root *Node) Contains(sequence []rune) bool {
+	if root != nil {
+		switch {
+		case len(sequence) > 1:
+			switch {
+			case root.child[sequence[0]] != nil:
+				return root.child[sequence[0]].Contains(sequence[1:])
+			default:
+				return false
+			}
+		case len(sequence) == 1:
+			switch {
+			case root.child[sequence[0]] != nil:
+				return true
+			default:
+				return false
+			}
+		default:
+			return false
+		}
+	}
+	return false
+}
+
+func (root *Node) Is_complete(sequence []rune) bool {
+	if root != nil {
+		switch {
+		case len(sequence) > 1:
+			switch {
+			case root.child[sequence[0]] != nil:
+				return root.child[sequence[0]].Is_complete(sequence[1:])
+			default:
+				return false
+			}
+		case len(sequence) == 1:
+			switch {
+			case root.child[sequence[0]] != nil:
+				return root.child[sequence[0]].final
+			default:
+				return false
+			}
+		default:
+			return false
+		}
+	} else {
+		return false
+	}
+}
