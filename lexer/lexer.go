d09a87ce6ed33511a765a48c23d27406eacd9b83
LArkadia <lubam1303@outlook.com>
2023-09-27 16:56:52 -0600

    Corrección de estilos Snake Case -> Cammel Case

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -7,40 +7,40 @@ import (
 	"os"
 )
 
-type tokens_agroupated struct {
-	Token_type string   `json:"type"`
-	Spaced     bool     `json:"spaced"`
-	Operators  []string `json:"operators"`
-	Operation  []string `json:"operation"`
+type tokensGrouped struct {
+	TokenType string   `json:"type"`
+	Spaced    bool     `json:"spaced"`
+	Operators []string `json:"operators"`
+	Operation []string `json:"operation"`
 }
-type raw_tokens struct {
-	Tokens []tokens_agroupated `json:"tokens"`
+type rawTokens struct {
+	Tokens []tokensGrouped `json:"tokens"`
 }
 
-func load_raw_tokens(json_source string) raw_tokens {
-	raw_tokens_file, err := os.ReadFile(json_source)
-	var json_tokens raw_tokens
+func loadRawTokens(jsonSource string) rawTokens {
+	rawTokensFile, err := os.ReadFile(jsonSource)
+	var jsonTokens rawTokens
 	if err != nil {
 		fmt.Println("Error while reading tokens file!!")
 	}
-	if err := json.Unmarshal(raw_tokens_file, &json_tokens); err != nil {
+	if err := json.Unmarshal(rawTokensFile, &jsonTokens); err != nil {
 		fmt.Println("Error while unmarshalling the tokens!!")
 	}
-	return json_tokens
+	return jsonTokens
 }
-func unwarp_tokens(json_tokens raw_tokens) (*trees.Node, map[rune]bool, map[string]string) {
-	tokens_sequences, non_spaced_tokens, tokens := new(trees.Node), make(map[rune]bool), make(map[string]string)
-	for _, token_group := range json_tokens.Tokens {
-		for i, token_operator := range token_group.Operators {
-			tokens[token_operator] = token_group.Token_type + token_group.Operation[i]
-			if !token_group.Spaced {
-				non_spaced_tokens[[]rune(token_operator)[0]] = true
+func unwrapTokens(jsonTokens rawTokens) (*trees.Node, map[rune]bool, map[string]string) {
+	tokensSequences, nonSpacedTokens, tokens := new(trees.Node), make(map[rune]bool), make(map[string]string)
+	for _, tokenGroup := range jsonTokens.Tokens {
+		for i, tokenOperator := range tokenGroup.Operators {
+			tokens[tokenOperator] = tokenGroup.TokenType + tokenGroup.Operation[i]
+			if !tokenGroup.Spaced {
+				nonSpacedTokens[[]rune(tokenOperator)[0]] = true
 			}
-			tokens_sequences.Add([]rune(token_operator))
+			tokensSequences.Add([]rune(tokenOperator))
 		}
 	}
-	return tokens_sequences, non_spaced_tokens, tokens
+	return tokensSequences, nonSpacedTokens, tokens
 }
-func Load_lexer_definition(json_source_path string) (*trees.Node, map[rune]bool, map[string]string) {
-	return unwarp_tokens(load_raw_tokens(json_source_path))
+func LoadLexerDefinition(jsonSourcePath string) (*trees.Node, map[rune]bool, map[string]string) {
+	return unwrapTokens(loadRawTokens(jsonSourcePath))
 }
--- lexer/trees/trees.go
+++ lexer/trees/trees.go
@@ -1,18 +1,19 @@
-//Archivo trees
 package trees
 
-type Node struct{
+//trees file
+
+type Node struct {
 	child map[rune]*Node
-	final bool	
+	final bool
 }
 
-func (root *Node) Add(sequence []rune)  {
-	if root.child == nil{
+func (root *Node) Add(sequence []rune) {
+	if root.child == nil {
 		root.child = make(map[rune]*Node)
 	}
-	switch{
+	switch {
 	case len(sequence) > 0:
-		if root.child[sequence[0]] == nil{
+		if root.child[sequence[0]] == nil {
 			root.child[sequence[0]] = new(Node)
 		}
 		root.child[sequence[0]].Add(sequence[1:])
@@ -46,13 +47,13 @@ func (root *Node) Contains(sequence []rune) bool {
 	return false
 }
 
-func (root *Node) Is_complete(sequence []rune) bool {
+func (root *Node) IsComplete(sequence []rune) bool {
 	if root != nil {
 		switch {
 		case len(sequence) > 1:
 			switch {
 			case root.child[sequence[0]] != nil:
-				return root.child[sequence[0]].Is_complete(sequence[1:])
+				return root.child[sequence[0]].IsComplete(sequence[1:])
 			default:
 				return false
 			}
@@ -71,12 +72,12 @@ func (root *Node) Is_complete(sequence []rune) bool {
 	}
 }
 
-func Get_numbers_tree()*Node{
-	root :=new(Node)
-	root.child=make(map[rune]*Node,0)
-	root.child['0']=new(Node)
+func GetNumbersTree() *Node {
+	root := new(Node)
+	root.child = make(map[rune]*Node)
+	root.child['0'] = new(Node)
 	//root.child['.']=root.child['0']
-	for i:='1'; i <= '9'; i++ {
+	for i := '1'; i <= '9'; i++ {
 		root.child[i] = root.child['0']
 	}
 	root.child['0'].final = true
@@ -110,7 +111,7 @@ func Get_numbers_tree()*Node{
 	return root
 }
 
-func Get_white_spaces() *Node {
+func GetWhiteSpaces() *Node {
 	root := new(Node)
 	root.child = make(map[rune]*Node)
 	root.child[' '] = new(Node)
