9c0d431670702367fecdf53f41000ce09baa5266
LArkadia <lubam1303@outlook.com>
2023-10-08 16:19:34 -0600

    added  Start, ID & add_tkns states

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strings"
 )
 
 type tokensGrouped struct {
@@ -43,4 +44,67 @@ func unwrapTokens(jsonTokens rawTokens) (*trees.Node, map[rune]bool, map[string]
 }
 func LoadLexerDefinition(jsonSourcePath string) (*trees.Node, map[rune]bool, map[string]string) {
 	return unwrapTokens(loadRawTokens(jsonSourcePath))
+}
+func Lexate(code []rune, tokens_sequences *trees.Node, non_spaced_tokens map[rune]bool, tokens_tag map[string]string, numbers *trees.Node, spaces *trees.Node) string {
+	var lexed_code strings.Builder
+	code = append(code, ' ')
+	buffer := make([]rune, 0)
+	pointer := 0
+	code_len := len(code)
+start:
+	for pointer < code_len && spaces.IsComplete(code[pointer:pointer+1]) {
+		pointer++
+	}
+	if pointer >= code_len {
+		goto end
+	}
+	switch {
+	case tokens_sequences.Contains(code[pointer : pointer+1]):
+		buffer = append(buffer, code[pointer])
+		pointer++
+		goto Op
+	case numbers.IsComplete(code[pointer : pointer+1]):
+		buffer = append(buffer, code[pointer])
+		pointer++
+		goto Num
+	case code[pointer] == '"':
+		pointer++
+		goto Str
+	default:
+		buffer = append(buffer, code[pointer])
+		pointer++
+		goto Id
+	}
+Op:
+Num:
+Str:
+Id:
+	switch {
+	case !spaces.Contains(code[pointer:pointer+1]) && !non_spaced_tokens[code[pointer]]:
+		buffer = append(buffer, code[pointer])
+		pointer++
+		goto Id
+	default:
+		goto add_tkn_Id
+
+	}
+	//Add_tokens
+add_tkn_OP:
+	lexed_code.WriteString(tokens_tag[string(buffer)] + "\n")
+	buffer = []rune{}
+	goto start
+add_tkn_Num:
+	lexed_code.WriteString("Number_" + string(buffer) + "\n")
+	buffer = []rune{}
+	goto start
+add_tkn_Str:
+	lexed_code.WriteString("String_" + string(buffer) + "\n")
+	buffer = []rune{}
+	goto start
+add_tkn_Id:
+	lexed_code.WriteString("ID_" + string(buffer) + "\n")
+	buffer = []rune{}
+	goto start
+end:
+	return lexed_code.String()
 }
